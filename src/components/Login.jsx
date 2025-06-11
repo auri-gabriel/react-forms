7baf7b2f93c26e7490af17ff117d59259ba277ba
Auri Gabriel <ririmello.a@gmail.com>
2025-06-11 17:56:47 -0300

    Fix email validation in Login component and update error handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from 'react';
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -8,6 +10,15 @@ export default function Login() {
     e.preventDefault();
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
 
     console.log(enteredEmail, enteredPassword);
   }
@@ -20,6 +31,9 @@ export default function Login() {
         <div className='control no-margin'>
           <label htmlFor='email'>Email</label>
           <input id='email' type='email' name='email' ref={email} />
+          <div className='control-error'>
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className='control no-margin'>
