2d539e6855f75e927a8ea91e57bf9606e21b3246
Auri Gabriel <ririmello.a@gmail.com>
2025-06-11 18:10:55 -0300

    Add password confirmation validation to Signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 export default function Signup() {
+  const [passwordsAreNotEqueal, setPasswordsAreNotEqueal] = useState(false);
+
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -6,6 +10,11 @@ export default function Signup() {
     const acquisitionChannel = fd.getAll('acquisition');
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+    if (data.password !== data['confirm-password']) {
+      setPasswordsAreNotEqueal(true);
+      return;
+    }
+
     console.log(data);
   }
 
@@ -39,6 +48,9 @@ export default function Signup() {
             name='confirm-password'
             required
           />
+          <div className='control-error'>
+            {passwordsAreNotEqueal && <p>Passwords must match.</p>}
+          </div>
         </div>
       </div>
 
